6884fb9cf84330cbfeb59f608742b96e02bcf991
Michael Conway <mike.conway@talktalk.net>
2025-11-26 17:47:49 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from blog.models import Post
 from blog.forms import CommentForm
 from django.shortcuts import render, get_object_or_404
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
 logger = logging.getLogger(__name__)
 
