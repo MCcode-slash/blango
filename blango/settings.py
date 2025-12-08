ae3f83dba190ce62316226000db55b5a0d3cf557
Michael Conway <mike.conway@talktalk.net>
2025-12-08 15:29:57 +0000

    Finish custom user models

--- blango/settings.py
+++ blango/settings.py
@@ -51,6 +51,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
+        'blango_auth',
         'blog',
         'debug_toolbar',
     ]
@@ -123,6 +124,8 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+
+    AUTH_USER_MODEL = "blango_auth.User"
 
     #logging
     LOGGING = {
