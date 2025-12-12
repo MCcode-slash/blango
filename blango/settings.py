89bb34eec453549e8fff8b4e8200ec19320b6234
Michael Conway <mike.conway@talktalk.net>
2025-12-12 11:43:08 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -48,12 +48,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'blango_auth',
         'blog',
         'debug_toolbar',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
 
     MIDDLEWARE = [
@@ -80,6 +85,8 @@ class Dev(Configuration):
                     'django.template.context_processors.request',
                     'django.contrib.auth.context_processors.auth',
                     'django.contrib.messages.context_processors.messages',
+                     # `allauth` needs this from django
+                    'django.template.context_processors.request',
                 ],
             },
         },
@@ -171,6 +178,14 @@ class Dev(Configuration):
     
     ACCOUNT_ACTIVATION_DAYS = 7
     #REGISTRATION_OPEN = False # to close the registration ability
+    # for allauth 
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    # allauth validation ACCOUNT_EMAIL_VERIFICATION = True
+    
+    SITE_ID = 1
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
