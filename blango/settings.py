f95f813329ca7e793628651fad211479792ce9d3
Michael Conway <mike.conway1001@gmail.com>
2025-12-19 15:04:26 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -60,6 +60,7 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
+        'rest_framework.authtoken',
     ]
 
     MIDDLEWARE = [
@@ -95,6 +96,13 @@ class Dev(Configuration):
 
     WSGI_APPLICATION = 'blango.wsgi.application'
 
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
 
     # Database
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
